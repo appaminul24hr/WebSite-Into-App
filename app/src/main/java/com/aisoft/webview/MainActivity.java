0706325209bca5c5ed09c5031042f6e0097560d0
appaminul24hr <appdeveloperaminul@gmail.com>
2024-03-09 22:39:43 +0600

    first commit

---
+++ app/src/main/java/com/aisoft/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.aisoft.webview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mwebView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        mwebView = findViewById(R.id.mwebView);
+        mwebView.getSettings().setJavaScriptEnabled(true);
+        mwebView.loadUrl("https://www.javatpoint.com/java-tutorial");
+
+
+    }
+}
